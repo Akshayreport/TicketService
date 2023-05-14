3e590e6ef271bce8b7efeca2766496cad47bf748
Akshayreport <49643455+Akshayreport@users.noreply.github.com>
2023-05-14 13:12:43 +0100

    Update TicketServiceTest.java

--- src-java/test/TicketServiceTest.java
+++ src-java/test/TicketServiceTest.java
@@ -1,29 +1,39 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
-public class TicketServiceTest {
 
-    @Test
-    public void testTicketPrices_DisplayCorrectly() {
-        String input = "2\n3\n1\n"; // Simulate user input: 2 adult tickets, 3 child tickets, 1 infant ticket
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+@Test
+class TicketServiceTest {
 
-        // Redirect console output for testing
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        System.setOut(printStream);
 
-        // Run the main method
-        DemoApplication.main(new String[]{});
+	@Test
+	public void testTicketPrices_DisplayCorrectly() {
+		String input = "2\n3\n1\n"; // Simulate user input: 2 adult tickets, 3 child tickets, 1 infant ticket
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
 
-        // Retrieve the console output
-        String consoleOutput = outputStream.toString();
+		// Redirect console output for testing
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(outputStream);
+		System.setOut(printStream);
 
-        // Verify if the correct prices are displayed
-        Assertions.assertTrue(consoleOutput.contains("Payment completed"));
-        Assertions.assertTrue(consoleOutput.contains("You paid £70")); // 2 adult tickets * £20 + 3 child tickets * £10
-    }
+		// Run the main method
+		TicketService.main(new String[]{});
+
+		// Retrieve the console output
+		String consoleOutput = outputStream.toString();
+
+		// Verify if the correct prices are displayed
+		Assertions.assertTrue(consoleOutput.contains("Payment completed"));
+		Assertions.assertTrue(consoleOutput.contains("You paid £70")); // 2 adult tickets * £20 + 3 child tickets * £10
+	}
+
 }
